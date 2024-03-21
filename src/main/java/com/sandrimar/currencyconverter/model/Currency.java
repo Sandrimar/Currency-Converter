e2fb8ceef6c61343cc8abed5f20d7e44c8e16e5f
Sandrimar Gibleski <sandrimarjose@outlook.com>
2024-03-21 13:38:59 -0300

    create currency entity

---
+++ src/main/java/com/sandrimar/currencyconverter/model/Currency.java
@@ -0,0 +1,77 @@
+package com.sandrimar.currencyconverter.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "moedas")
+public class Currency {
+
+    @Id
+    private String code;
+    @Column(precision = 22, scale = 12)
+    private BigDecimal value;
+    private Instant lastUpdate;
+    private boolean available;
+
+    public Currency() {
+    }
+
+    public Currency(String code, BigDecimal value, Instant lastUpdate, boolean available) {
+        this.code = code;
+        this.value = value;
+        this.lastUpdate = lastUpdate;
+        this.available = available;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currency currency = (Currency) o;
+        return Objects.equals(code, currency.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
+}
